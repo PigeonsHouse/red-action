70033c9282e00629b44662ff83e07adde2a8b629
sanshi3412F <m.kento.hr5500718@gmail.com>
2020-07-09 21:37:39 +0900

    Stageroommaking

---
+++ Assets/Script/Sanshi/Warpsystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warpsystem : MonoBehaviour
+{   
+    void OnControllerColliderHit(ControllerColliderHit hit){
+        if (hit.gameObject.tag == "Warp") {
+            GetComponent<Hero>().enabled = false;
+            transform.position = new Vector3(20,-1,0);
+            GetComponent<Hero>().enabled = true;
+            Debug.Log(hit.gameObject);
+        }
+
+    }
+}
